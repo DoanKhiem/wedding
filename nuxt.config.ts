00006f8275a1e02a2d5106add141acc93ace717b
doankhiem <khiembi161219@gmail.com>
2024-10-06 16:17:39 +0700

    add template

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    '~/public/css/bootstrap.min.css',
+    '~/public/css/pogo-slider.min.css',
+    '~/public/css/style.css',
+    '~/public/css/responsive.css',
+    '~/public/css/custom.css',
+  ],
+  app: {
+    head: {
+      script: [
+        { src: '/js/jquery.min.js', type: 'text/javascript' },
+        { src: '/js/popper.min.js', type: 'text/javascript' },
+        { src: '/js/bootstrap.min.js', type: 'text/javascript' },
+        { src: '/js/jquery.magnific-popup.min.js', type: 'text/javascript' },
+        { src: '/js/jquery.pogo-slider.min.js', type: 'text/javascript' },
+        { src: '/js/slider-index.js', type: 'text/javascript' },
+        { src: '/js/form-validator.min.js', type: 'text/javascript' },
+        { src: '/js/contact-form-script.js', type: 'text/javascript' },
+        { src: '/js/custom.js', type: 'text/javascript' },
+        
+      ]
+    }
+  }
 })
