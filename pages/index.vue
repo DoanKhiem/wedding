c6ce7ad3e22c332e438892e0ce81eacec9c89f24
DESKTOP-GBO2TA6\pc <Hieunghe1712@gmail.com>
2024-10-11 20:16:55 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -342,11 +342,11 @@
             <div class="map-container d-flex justify-content-between">
                 <div class="map-location mx-3">
                     <h1 class="text-center">Nhà trai</h1>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1965.086729044587!2d105.69981961870232!3d19.260775094413745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTnCsDE1JzM3LjMiTiAxMDXCsDQyJzAzLjkiRQ!5e1!3m2!1svi!2s!4v1728577319434!5m2!1svi!2s" width="100%" height="500" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d941.6264905281023!2d105.70043326962538!3d19.26035126491649!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTnCsDE1JzM3LjMiTiAxMDXCsDQyJzAzLjkiRQ!5e0!3m2!1svi!2s!4v1728652523373!5m2!1svi!2s" width="100%" height="500" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div class="map-location mx-3">
                     <h1 class="text-center">Nhà gái</h1>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d4674.888686768101!2d105.71026107596774!3d19.222201047365033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTnCsDEzJzE5LjkiTiAxMDXCsDQyJzQ2LjIiRQ!5e1!3m2!1svi!2s!4v1728577555657!5m2!1svi!2s" width="100%" height="500" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3767.3814757564123!2d105.71025837596775!3d19.22219944736519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTnCsDEzJzE5LjkiTiAxMDXCsDQyJzQ2LjIiRQ!5e0!3m2!1svi!2s!4v1728652383888!5m2!1svi!2s" width="100%" height="500" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
             <!-- <MapComponent /> -->
